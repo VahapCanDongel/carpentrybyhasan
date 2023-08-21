2a0081b361441114ad286a5bfe8a8b6004453980
v-Alqorith <vahapcandongel@hotmail.com>
2023-08-21 18:50:07 +0100

    changed testimonials

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -6,37 +6,39 @@ export default function Testimonials() {
                 Testimonials
             </div>
 
-            <div className="flex flex-col gap-4 mr-auto justify-between w-full mt-6">
-                <div className="flex gap-[200px]  w-full sm:flex-col sm:justify-center sm:items-center justify-start">
-                    <div className="w-[350px] h-[300px] rounded-sm flex flex-col justify-center items-center text-[15px] ">
+            <div className="flex flex-wrap gap-4 mr-auto justify-between w-full mt-6">
+                <div className="w-[350px] h-[280px] rounded-sm flex flex-col justify-between items-center text-[15px] ">
+                    <div>
                         Working with Carpentry By Hasan was an absolute pleasure. From the initial consultation to the final installation, their attention to detail was impeccable. They took my rough ideas for a custom bookshelf and turned them into a stunning reality that seamlessly blended with the rest of my home's design. It's evident that they put their heart and soul into every piece. Highly recommended for anyone who values quality and craftsmanship."
 
-                        <div className="mr-auto font-bold mt-4">XYZ Company</div>
-
-
                     </div>
-                    <div className="w-[350px] h-[300px] rounded-sm flex flex-col justify-center items-center text-[15px] ">
+
+                    <div className="mr-auto font-bold mt-4 mb-8">XYZ Company</div>
+
+
+                </div>
+                <div className="w-[350px] h-[280px] rounded-sm flex flex-col justify-between items-center text-[15px] ">
+                    <div>
                         I've had my fair share of dealings with carpenters over the years, but none have stood out quite like Carpentry By Hasan. Their team's dedication to preserving the authenticity of our heritage home while infusing modern touches was remarkable. They respected the history, yet weren’t afraid to innovate. Our newly restored wooden staircase is a testament to their skill and dedication.
-
-                        <div className="mr-auto font-bold mt-4">XYZ Company</div>
                     </div>
+
+                    <div className="mr-auto font-bold mt-4 mb-8">XYZ Company</div>
                 </div>
 
-                <div className="flex gap-[200px]  w-full sm:flex-col sm:justify-center sm:items-center justify-start">
-                    <div className="w-[350px] h-[300px] rounded-sm flex flex-col justify-center items-center text-[15px] ">
-                        Having Carpentry By Hasan craft a custom dining table for our family was a decision we’ll always cherish. It’s not just a piece of furniture; it’s a piece of art. Every dinner, every family gathering, we’re reminded of the craftsmanship and love that went into it. They took the time to understand our needs and preferences, resulting in a centerpiece that will be in our family for generations
-
-                        <div className="mr-auto font-bold mt-4">XYZ Company</div>
-
-
+                <div className="w-[350px] h-[280px] rounded-sm flex flex-col justify-between items-center text-[15px] mt-8">
+                    <div>
+                        Having Carpentry By Hasan craft a custom dining table for our family was a decision we’ll always cherish. It’s not just a piece of furniture; it’s a piece of art. Every dinner, every family gathering, we’re reminded of the craftsmanship and love that went into it. They took the time to understand our needs and preferences, resulting in a centerpiece that will be in our family for generations.
                     </div>
-                    <div className="w-[350px] h-[300px] rounded-sm flex flex-col justify-center items-center text-[15px] ">
+
+                    <div className="mr-auto font-bold mt-4 mb-8">XYZ Company</div>
+
+
+                </div>
+                <div className="w-[350px] h-[280px] rounded-sm flex flex-col justify-between items-center text-[15px] mt-8">
+                    <div>
                         We hired Carpentry By Hasan for a complete kitchen overhaul, and they exceeded all our expectations. Every drawer, cabinet, and shelf was meticulously planned and executed. Their understanding of wood, its nuances, and how it interacts in a living space is unparalleled. Now, our kitchen isn't just a place to cook; it's the heart of our home, all thanks to their exceptional work.
-
-                        <div className="mr-auto font-bold mt-4">XYZ Company</div>
-
-
                     </div>
+                    <div className="mr-auto font-bold mt-4 mb-8">XYZ Company</div>
                 </div>
             </div>
 
