aef09243f71f7c3466f7639154c768e9d6e7144d
v-Alqorith <vahapcandongel@hotmail.com>
2023-08-22 19:28:37 +0100

    changed width on small screens

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,6 +1,6 @@
 export default function TestimonialCard({ testimonial_paragraph, user }) {
     return (
-        <div className="w-[350px] h-[280px] rounded-sm flex flex-col justify-between items-center text-[15px]  sm:w-[400px] my-9">
+        <div className="w-[350px] h-[280px] rounded-sm flex flex-col justify-between items-center text-[15px]  sm:w-[400px] my-9 sm:h-[200px]">
             <div>
                 {testimonial_paragraph}
             </div>
