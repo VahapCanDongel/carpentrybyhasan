c02ded9b200d086fe43966215882592f73831467
v-Alqorith <vahapcandongel@hotmail.com>
2023-08-22 19:19:42 +0100

    added pointer

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -8,12 +8,12 @@ export default function ServiceCard({ service_info, service_paragraph }) {
 
     return (
         <div>
-            <div className="bg-gray-300 w-[250px] sm:w-[400px] h-[300px] rounded-sm flex justify-center items-center text-[18px] font-bold xl:w-full lg:w-1/3 lg:h-[200px] lg:text-lg" onClick={toggleModal}>
+            <div className="cursor-pointer bg-gray-300 w-[250px] sm:w-[400px] h-[300px] rounded-sm flex justify-center items-center text-[18px] font-bold xl:w-full lg:w-1/3 lg:h-[200px] lg:text-lg" onClick={toggleModal}>
                 {service_info}
             </div>
 
             {modalState && (
-                <div className="w-full h-screen flex justify-center items-center fixed top-0 left-0 z-50 bg-opacity-50 bg-black" >
+                <div className="w-full h-screen flex justify-center items-center fixed top-0 left-0 z-50 bg-opacity-50 bg-black " >
                     <div className="w-[800px]  bg-slate-100 sm:w-[400px] sm:h-[600px] flex flex-col p-10 rounded-md">
                         <div className=" font-semibold text-[23px]">
                             {service_info}
